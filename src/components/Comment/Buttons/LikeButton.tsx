87a80b9bbc0f8417aa27e2df05d1564631d4e44e
extroblade <mr.ovs0ford@gmail.com>
2023-06-22 23:53:43 +0400

    remove old enums

--- src/components/Comment/Buttons/LikeButton.tsx
+++ src/components/Comment/Buttons/LikeButton.tsx
@@ -2,13 +2,12 @@ import React, { FC, useState } from 'react';
 import styles from '@/components/Comment/Comment.module.scss';
 import { AiFillLike, AiOutlineLike } from 'react-icons/ai';
 import { Button } from '@/components/Button/Button';
-import { BtnA } from '@/components/Button/Button.props';
 
 const LikeButton: FC = (): JSX.Element => {
   const [like, setLike] = useState(false);
   return (
     <Button
-      appearance={BtnA.transparent}
+      appearance={'transparent'}
       className={`${styles.buttonUp} ${like ? styles.active : ''}`}
       onClick={() => setLike((l) => !l)}
     >
