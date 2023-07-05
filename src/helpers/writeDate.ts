bcd3b39bc3f3d718956d7ec55ea8adf4f96c3f28
extroblade <mr.ovs0ford@gmail.com>
2023-07-05 17:17:16 +0400

    add: fetch reviews

--- src/helpers/writeDate.ts
+++ src/helpers/writeDate.ts
@@ -1,4 +1,4 @@
-export const writeDate = (date: number): string => {
+export const writeDate = (date: number | string): string => {
   //DD.MM.YYYY
   const now: Date = new Date(date);
   const day = now.getDate();
