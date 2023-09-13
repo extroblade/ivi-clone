04fc20145134e5829e59973e06221f2ede23141b
extroblade <mr.ovs0ford@gmail.com>
2023-09-13 16:20:48 +0400

    fix: awards

--- src/components/Modals/WatchPageModal/Tabs/AwardsTab.tsx
+++ src/components/Modals/WatchPageModal/Tabs/AwardsTab.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useAppSelector } from '@/hooks/redux';
 import { selectModal } from '@/store/reducers/modals.slice';
 import { Htag } from '@/UI/Htag/Htag';
+import Image from 'next/image';
+import { P } from '@/UI/P/P';
 
 const AwardsTab = () => {
   const { currentMovie } = useAppSelector(selectModal);
@@ -9,7 +11,13 @@ const AwardsTab = () => {
   return (
     <div>
       {awards?.length ? (
-        awards.map((award: string) => <div key={award}>{award}</div>)
+        awards.map((award: Record<string, string>) => (
+          <div key={award.name}>
+            <Htag tag={'h3'}>{award.name}</Htag>
+            <P>{award.nominationName}</P>
+            <Image src={award.imageUrl} width={100} height={100} alt={award.name} />
+          </div>
+        ))
       ) : (
         <Htag tag={'h2'}>Награды не указаны</Htag>
       )}
